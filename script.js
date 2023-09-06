2af67b92d6584f6e6f9aa2063b31d7fb4fbb55d3
Stefan Land <stefanland59@gmail.com>
2023-09-06 15:07:05 -0700

    added buttons to toggle read status and delete

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read, info) {
+function Book(title, author, pages, read, info, toggleRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function() {
         return title + " by " + author + ", " + pages + ", " + "read? " + read; 
+    }
+    this.toggleRead = function() {
+        if(this.read === 'Yes') {
+            this.read = 'No';
+        }
+
+        else if(this.read === 'No') {
+            this.read = 'Yes';
+        }
     }
 }
 
@@ -19,41 +28,25 @@ function addBookToLibrary() {
 
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
-
-    const displayNewBook = myLibrary.slice(-1);
-    const table = document.querySelector('.book-table');
-    displayNewBook.forEach((b) => {
-        const row = document.createElement('tr');
-        
-        const titleCell = document.createElement('td');
-        const authorCell = document.createElement('td');
-        const pagesCell = document.createElement('td');
-        const readCell = document.createElement('td');
-
-        titleCell.textContent = b.title;
-        row.appendChild(titleCell);
-
-        authorCell.textContent = b.author;
-        row.appendChild(authorCell);
-
-        pagesCell.textContent = b.pages;
-        row.appendChild(pagesCell);
-
-        readCell.textContent = b.read;
-        row.appendChild(readCell);
-        table.appendChild(row);
-    })
+    
+    clearTable();
+    displayBooks();
 }
 
 function displayBooks() {
     const table = document.querySelector('.book-table');
-    myLibrary.forEach((b) => {
+    myLibrary.forEach((b, index) => {
         const row = document.createElement('tr');
+        row.classList.add('book-row');
         
         const titleCell = document.createElement('td');
         const authorCell = document.createElement('td');
         const pagesCell = document.createElement('td');
         const readCell = document.createElement('td');
+        const deleteCell = document.createElement('td');
+        const deleteButton = document.createElement('button');
+        const readToggleCell = document.createElement('td');
+        const readToggleButton = document.createElement('button');
 
         titleCell.textContent = b.title;
         row.appendChild(titleCell);
@@ -66,8 +59,40 @@ function displayBooks() {
 
         readCell.textContent = b.read;
         row.appendChild(readCell);
+
+        readToggleButton.textContent = 'Read?';
+        readToggleButton.setAttribute('data-index-number', index);
+        readToggleButton.addEventListener('click', () => {
+            const index = readToggleButton.getAttribute('data-index-number');
+            myLibrary[index].toggleRead();
+            clearTable();
+            displayBooks();
+        })
+
+        row.appendChild(readToggleCell);
+        readToggleCell.appendChild(readToggleButton);
+
+        deleteButton.textContent = 'Delete Book';
+        deleteButton.setAttribute('data-index-number', index);
+        deleteButton.addEventListener('click', () => {
+            deleteBook(deleteButton.getAttribute('data-index-number'));
+        })
+        row.appendChild(deleteCell);
+        
+        deleteCell.appendChild(deleteButton);
         table.appendChild(row);
     })
+}
+
+function deleteBook(index) {
+    myLibrary.splice(index, 1);
+    console.log(myLibrary);
+    clearTable();
+    displayBooks();
+}
+
+function clearTable() {
+   document.querySelectorAll('.book-row').forEach(e => e.remove());
 }
 
 const form = document.getElementById('form');
@@ -83,14 +108,11 @@ addBook.addEventListener("click", () => {
     showForm.showModal();
 })
 
-const book1 = new Book("The Hobbit", "J.R. Tolkien", 295, "yes");
-const book2 = new Book("The Hobbit", "J.R. Tolkien", 295, "yes");
-const book3 = new Book("The Hobbit", "J.R. Tolkien", 295, "yes");
-const book4 = new Book("The Hobbit", "J.R. Tolkien", 295, "yes");
-const book5 = new Book("The Hobbit", "J.R. Tolkien", 295, "yes");
-
-console.log(typeof book1);
-console.log(book1.title);
+const book1 = new Book("The Hobbit1", "J.R. Tolkien", 295, "Yes");
+const book2 = new Book("The Hobbit2", "J.R. Tolkien", 295, "No");
+const book3 = new Book("The Hobbit3", "J.R. Tolkien", 295, "Yes");
+const book4 = new Book("The Hobbit4", "J.R. Tolkien", 295, "No");
+const book5 = new Book("The Hobbit5", "J.R. Tolkien", 295, "Yes");
 
 myLibrary.push(book1);
 myLibrary.push(book2);
@@ -99,3 +121,4 @@ myLibrary.push(book4);
 myLibrary.push(book5);
 
 displayBooks();
+
